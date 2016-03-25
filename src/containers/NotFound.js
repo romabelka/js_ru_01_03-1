1055fd2729011e4ea77e335a0822d14acac39ce3
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-03-25 18:54:23 +0200

    some Routes API

---
+++ src/containers/NotFound.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react'
+
+class NotFound extends Component {
+    static propTypes = {
+
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Not Found</h1>
+            </div>
+        )
+    }
+}
+
+export default NotFound
--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,26 @@
 import React from 'react'
-import { Router, Route, browserHistory, hashHistory } from 'react-router'
+import { Router, Route, browserHistory, Redirect, IndexRedirect } from 'react-router'
 import App from './containers/App'
 import Articles from './containers/Articles'
 import ArticlePage from './containers/ArticlePage'
 import NewArticle from './containers/NewArticle'
 import CommentsIndex from './containers/CommentIndex'
 import CommentsPaginationPage from './containers/CommentsPaginationPage'
+import NotFound from './containers/NotFound'
 
 export default (
     <Router history = {browserHistory}>
         <Route path="/" component = {App}>
-            <Route path ="/articles" component = {Articles}>
+            <Redirect from="article" to="articles" />
+            <Route path ="articles" component = {Articles}>
                 <Route path = "new" component = {NewArticle} />
                 <Route path = ":id" component = {ArticlePage} />
             </Route>
-            <Route path="/comments" component = {CommentsIndex}>
+            <Route path="comments" component = {CommentsIndex}>
+                <IndexRedirect to="1"/>
                 <Route path = ":page" component = {CommentsPaginationPage} />
             </Route>
+            <Route path="*" component={NotFound} />
         </Route>
     </Router>
 )
